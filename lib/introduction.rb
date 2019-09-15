13a55c631b1c3c3ade9162504237559c4ecfd4b8
Robinson Santana Silverio <robinsntn@gmail.com>
2019-09-15 22:39:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,7 +8,8 @@ def introduction(name)
 end
 
 def introduction_with_language(name, language)
-  puts "hi, my name is #{name} and I am learning to program in #"
+  puts "hi, my name is #{name} and I am learning to program in #{language}"
+end
 
 
 
